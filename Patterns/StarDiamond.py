c68f17945530aa8e4bd090a3fb8b9763b5e3ddc1
Sivatejach <chsteja2003@gmail.com>
2025-05-01 22:49:38 +0530

    Update Patterns

---
+++ Patterns/StarDiamond.py
@@ -0,0 +1,12 @@
+
+
+
+n=int(input("Enter the number of elements in the list: "))
+
+
+for i in range(1,n*2,2):
+    print(" "*(2*n-i-1)+"*"*i+" "*(2*n-i-1))
+
+
+for i in range(2*n-1,0,-2):
+    print(" "*(2*n-i-1)+"*"*i+" "*(2*n-i-1))
