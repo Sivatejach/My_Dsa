1518ff6ccee36d44e265446641ec20e2f9bf2ede
Sivatejach <chsteja2003@gmail.com>
2025-05-01 12:33:15 +0530

    Update Patterns

--- Patterns/numTriangle.py
+++ Patterns/numTriangle.py
@@ -1,11 +1,10 @@
 
 
-
 n=int(input("Enter the number of elements in the list: "))
 strval=""
 for i in range(1,n+1):
     strval=""
     for j in range(1,i+1):
-        strval+=str(j)+" "
+        strval+=str(i)+" "
     print(strval)
     
