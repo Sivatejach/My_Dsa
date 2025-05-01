b1ec9fb6a07e0b56991a25db62195c418f609be1
Sivatejach <chsteja2003@gmail.com>
2025-05-01 12:01:32 +0530

    Update DP

---
+++ DP/MaximizeSumofSubarrayDifferences.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class MaximizeSumofSubarrayDifferences {
+
+    public static void maximizeSumOfSubarrayDifferences(int[] arr, int n) {
+        //Incomplete code
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        maximizeSumOfSubarrayDifferences(arr, n);
+        sc.close();
+        // Output the result
+        // System.out.println(result);
+        // The result should be the maximum sum of absolute differences of all subarrays.
+
+    }
+}
