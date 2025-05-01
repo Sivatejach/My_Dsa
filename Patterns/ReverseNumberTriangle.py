86066eb91fec2a9b8d877045c50aab8f424aa6ee
Sivatejach <chsteja2003@gmail.com>
2025-05-01 12:39:30 +0530

    Update Patterns

---
+++ Patterns/ReverseNumberTriangle.py
@@ -0,0 +1,12 @@
+
+
+
+n=int(input("Enter the number of elements in the list: "))
+strval=""
+for i in range(n,0,-1):
+    strval=""
+    for j in range(1,i+1):
+        strval+=str(j)+" "
+    print(strval)
+
+
