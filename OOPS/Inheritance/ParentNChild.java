86a4d86dd66ad9021e52432ffa164e6682c46331
Sivatejach <chsteja2003@gmail.com>
2025-05-06 22:09:50 +0530

    Updated Code

--- OOPS/Inheritance/ParentNChild.java
+++ OOPS/Inheritance/ParentNChild.java
@@ -1,47 +1,50 @@
 package OOPS.Inheritance;
+
 // Parent class
-class Parent{
-    String name;
-    int age;
-    String address;
-    public Parent(String name, int age, String address) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
+class Animal{
+    String breed;
+
+    public void sound(){
+        System.out.println("Animal makes sound");
     }
-    public void displayInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Address: " + address);
+
+    public void eat(){
+        System.out.println("Animal eats food");
     }
 }
+
+
 // Child class
-class Child extends Parent{
-    String schoolName;
-    String grade;
-    public Child(String name, int age, String address, String schoolName, String grade) {
-        super(name, age, address); 
-        this.schoolName = schoolName;
-        this.grade = grade;
+class Dog extends Animal{
+    String name="Dog";
+
+    public void sound(){
+        System.out.println("Dog barks");
     }
-    public void displayInfo() {
-        super.displayInfo(); 
-        System.out.println("School Name: " + schoolName);
-        System.out.println("Grade: " + grade);
-    }
-    public void study() {
-        System.out.println(name + " is studying in " + schoolName + " in grade " + grade + ".");
+
+    public void eat(){
+        System.out.println("Dog eats dog food");
     }
 }
 public class ParentNChild {
     public static void main(String[] args) {
-        Parent parent = new Parent("John Doe", 45, "123 Main St");
-        parent.displayInfo();
-        System.out.println("*****************************");
-        Child child = new Child("Jane Doe", 15, "123 Main St", "ABC High School", "10th Grade");
-        child.displayInfo();
-        System.out.println("*****************************");
-        child.study();
+        Animal animal = new Animal();
+        animal.sound(); // Output: Animal makes sound
+        animal.eat();   // Output: Animal eats food
 
+        System.out.println("*****************************");
+
+        Dog dog = new Dog();
+        dog.sound();    // Output: Dog barks
+        dog.eat();      // Output: Dog eats dog food
+
+        System.out.println("*****************************");
+
+        // Upcasting
+        Animal myDog = new Dog();
+        myDog.sound();  // Output: Dog barks (overridden method)
+        myDog.eat();    // Output: Dog eats dog food (overridden method)
+
+        System.out.println("*****************************");
     }
 }
