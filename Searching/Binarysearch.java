cf2ee6e1197afcd563596f8d0c712d0b4038be28
Sivatejach <chsteja2003@gmail.com>
2025-05-06 19:51:34 +0530

    Updated Code

--- Searching/Binarysearch.java
+++ Searching/Binarysearch.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 
 public class Binarysearch {
     public static int binarySearch(int arr[], int target){
-        int left=0;
-        int right=arr.length-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
+        int l=0;
+        int r=n-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
             if(arr[mid]==target){
                 return mid;
             }
             else if(arr[mid]<target){
-                left=mid+1;
+                l=mid+1;
             }
             else{
-                right=mid-1;
+                r=mid-1;
             }
         }
-        return -1;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
