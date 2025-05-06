07e19f7f17a235f729edc2e45517e9d5982ba2fb
Sivatejach <chsteja2003@gmail.com>
2025-05-06 22:35:35 +0530

    Updated Code

---
+++ OOPS/Polymorphism/CompilePoly.java
@@ -0,0 +1,25 @@
+package OOPS.Polymorphism;
+
+
+class MathUtils {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class CompilePoly {
+    public static void main(String[] args) {
+        MathUtils mu = new MathUtils();
+        System.out.println(mu.add(2, 3));          // 5
+        System.out.println(mu.add(2.5, 3.5));      // 6.0
+        System.out.println(mu.add(1, 2, 3));       // 6
+    }
+}
