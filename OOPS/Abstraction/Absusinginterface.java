40e37fa9207330397271829ba98f1ed0248548d0
Sivatejach <chsteja2003@gmail.com>
2025-05-06 22:27:15 +0530

    Updated Code

---
+++ OOPS/Abstraction/Absusinginterface.java
@@ -0,0 +1,24 @@
+package OOPS.Abstraction;
+
+
+abstract class Vehicle {
+    abstract void startEngine();
+    void fuel() {
+        System.out.println("Fueling the vehicle...");
+    }
+}
+
+class Car extends Vehicle {
+    @Override
+    void startEngine() {
+        System.out.println("Car engine started.");
+    }
+}
+
+public class Absusinginterface {
+    public static void main(String[] args) {
+        Vehicle myCar = new Car();
+        myCar.startEngine(); // Abstract method implemented
+        myCar.fuel();         // Concrete method from abstract class
+    }
+}
