58f69ff38497174c4d33603524bc4cdf6a50790d
Sivatejach <chsteja2003@gmail.com>
2025-05-06 15:39:36 +0530

    Updated Code

--- CodeRush/LamdaImpl.java
+++ CodeRush/LamdaImpl.java
@@ -7,18 +7,16 @@ public class LamdaImpl {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements in the array: ");
         int n = sc.nextInt();
-        int arr[] = new int[n];
+        Integer arr[] = new Integer[n];
         System.out.println("Enter the elements of the array: ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr, (a, b) -> a - b);
+        Arrays.sort(arr, (a, b) -> a - b); // Using lambda expression to sort the array
         System.out.println("Sorted array: ");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
-        System.out.println();
-        sc.close();
         
     }    
 }
