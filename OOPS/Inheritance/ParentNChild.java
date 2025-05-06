274ce0927a65ff7398a728d2d40b5b7c90ea2f2d
Sivatejach <chsteja2003@gmail.com>
2025-05-06 22:04:16 +0530

    Updated Code

---
+++ OOPS/Inheritance/ParentNChild.java
@@ -0,0 +1,47 @@
+package OOPS.Inheritance;
+// Parent class
+class Parent{
+    String name;
+    int age;
+    String address;
+    public Parent(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+    }
+}
+// Child class
+class Child extends Parent{
+    String schoolName;
+    String grade;
+    public Child(String name, int age, String address, String schoolName, String grade) {
+        super(name, age, address); // Call the constructor of the parent class
+        this.schoolName = schoolName;
+        this.grade = grade;
+    }
+    public void displayInfo() {
+        super.displayInfo(); // Call the displayInfo method of the parent class
+        System.out.println("School Name: " + schoolName);
+        System.out.println("Grade: " + grade);
+    }
+    public void study() {
+        System.out.println(name + " is studying in " + schoolName + " in grade " + grade + ".");
+    }
+}
+public class ParentNChild {
+    public static void main(String[] args) {
+        Parent parent = new Parent("John Doe", 45, "123 Main St");
+        parent.displayInfo();
+        System.out.println("*****************************");
+        Child child = new Child("Jane Doe", 15, "123 Main St", "ABC High School", "10th Grade");
+        child.displayInfo();
+        System.out.println("*****************************");
+        child.study();
+
+    }
+}
