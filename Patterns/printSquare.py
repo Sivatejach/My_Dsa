b9d3102608bab84ed10e8cee94dec836ea1472f6
Sivatejach <chsteja2003@gmail.com>
2025-10-06 16:44:08 +0530

    Patterns updated

---
+++ Patterns/printSquare.py
@@ -0,0 +1,17 @@
+
+
+
+n=int(input("Enter the number of elements in the list: "))
+for i in range(n):
+    print("* "*n)
+
+
+#Sample Input:
+# 5
+#Sample Output:
+# * * * * *
+# * * * * * 
+# * * * * *
+# * * * * *
+# * * * * *
+
