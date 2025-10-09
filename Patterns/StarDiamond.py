2970561f36864413dba0e945922f499e9bf48d52
Sivatejach <chsteja2003@gmail.com>
2025-10-09 09:07:19 +0530

    Striver's Patterns

--- Patterns/StarDiamond.py
+++ Patterns/StarDiamond.py
@@ -10,3 +10,11 @@ for i in range(1,n*2,2):
 
 for i in range(2*n-1,0,-2):
     print(" "*(2*n-i-1)+"*"*i+" "*(2*n-i-1))
+
+#Sample Input:
+# 5
+#Sample Output:
+#         *
+#        ***
+#       *****
+#      *******
