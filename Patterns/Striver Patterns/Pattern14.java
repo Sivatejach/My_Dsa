5e9c7fffd336ccedb692226ee9b69939134a630d
Sivatejach <chsteja2003@gmail.com>
2025-10-09 17:32:17 +0530

    Patterns added

--- Patterns/Striver Patterns/Pattern14.java
+++ Patterns/Striver Patterns/Pattern14.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
-public class Pattern13 {
+public class Pattern14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         int n = sc.nextInt();
-        int val=1;
+        char ch;
         for(int i=0;i<n;i++){
+            ch='A';
             for(int j=0;j<=i;j++){
-                System.out.print(val+" ");
-                val++;
+                System.out.print(ch+" ");
+                ch++;
+                if(ch>'Z')
+                    ch='A';
             }
             System.out.println();
         }
-        sc.close();
-    }
+    }    
 }
