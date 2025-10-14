d64668905afac3e937132e9031594330b1cd5a7f
Sivatejach <chsteja2003@gmail.com>
2025-10-14 10:26:19 +0530

    GCD Added

---
+++ Basic Math/gcdorhcf.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class gcdorhcf {
+    public static int gcd(int a, int b) {
+        if(a>b){
+            int temp=a;
+            a=b;
+            b=temp;
+        }
+        for(int i=a;i>=1;i--){
+            if(a%i==0 && b%i==0){
+                return i;
+            }
+        }
+        return 1; // If no common divisor is found, return 1
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);   
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        int result = gcd(num1, num2);
+        System.out.println("The GCD (HCF) of " + num1 + " and " + num2 + " is: " + result);
+        scanner.close();
+    }
+}
