0aae0402c570635865982ec023a5b510f6876b58
Sivatejach <chsteja2003@gmail.com>
2025-10-15 11:56:26 +0530

    Problems in recursion added

--- BasicRecursion/FunctionalWay/reverseanArray.java
+++ BasicRecursion/FunctionalWay/reverseanArray.java
@@ -22,6 +22,12 @@ public class reverseanArray {
         for(int i=0; i<n; i++) {
             System.out.print(arr[i] + " ");
         }
+        reverseanArrayusingrecursionandbacktracking(arr,0,n-1);
+        System.out.println();
+        System.out.println("Reversed array using recursion & backtracking: ");
+        for(int i=0; i<n; i++) {
+            System.out.print(arr[i] + " ");
+        }
         
     }
     //In this function the array will be reversed because we are swapping the values in the array
@@ -29,8 +35,10 @@ public class reverseanArray {
         if(l>=r) return ;
         swap(arr, l, r);
         reverseanArrayusingrecursion(arr,l+1,r-1);
+        
 
     }
+
     //In the below function the array will not be reversed because we are swapping the values of a and b but not the values in the array
     public static void reverseanArrayusingfunctioncall(int[] arr,int l,int r){
         while(l<r){
@@ -45,6 +53,11 @@ public class reverseanArray {
             swap(arr, i, n - i - 1);
             reveranArrayusingsinglepointer(arr,i+1);
     }
+    public static void reverseanArrayusingrecursionandbacktracking(int[] arr,int l,int r){
+        if(l>=r) return ;
+        reverseanArrayusingrecursionandbacktracking(arr,l+1,r-1);
+        swap(arr, l, r);
+    }
     //We must pass array as parameter because in java primitive data types are passed by value and non-primitive data types are passed by reference
     //So if we pass array as parameter then the changes made in the function will be reflected in the original array
     //If we don't pass array as parameter then the changes made in the function will not be reflected in the original array
@@ -54,5 +67,5 @@ public class reverseanArray {
         arr[j] = temp;
     }
 
-
 }
+
